951ae4a1fca34d724ffdd06aec652f093e835380
nielskerdel <nielskerdel@host.com>
2017-03-17 17:14:52 +0100

    config files

--- src/main/java/B2a/config/SpringWebConfig.java
+++ src/main/java/B2a/config/SpringWebConfig.java
@@ -1,4 +1,16 @@
 package B2a.config;
 
-public class SpringWebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class SpringWebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
+
 }
--- src/main/java/B2a/config/WebSecurityConfig.java
+++ src/main/java/B2a/config/WebSecurityConfig.java
@@ -35,6 +35,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                             "/resources/**",
                             "/",
                             "/home",
+                            "/newsmessage",
                             "/registration",
                             "/shop",
                             "/ticketOrder",
