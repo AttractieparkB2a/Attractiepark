2f17dd4c81799c436232ece2680cd40de15430e8
Bart Helleman <bart_helleman@hotmail.nl>
2017-03-17 13:48:35 +0100

    Updated authentication views

---
+++ src/main/java/B2a/config/SpringWebConfig.java
@@ -0,0 +1,15 @@
+package B2a.config;
+
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+        import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class SpringWebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
+}
