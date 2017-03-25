7614b969811d8aad02c79cad0c79c65cdf717959
nielskerdel <nielskerdel@host.com>
2017-03-25 15:41:38 +0100

    Responsive footer

--- src/main/resources/static/js/nav.js
+++ src/main/resources/static/js/nav.js
@@ -1,3 +1,11 @@
-/**
- * Created by Niels on 24-3-2017.
- */
+$(document).ready(function(){
+    $('#hamburger').click(function(){
+        $(this).toggleClass('open');
+        $('.responsive-navigation').toggleClass('visible');
+    });
+
+    $('.menu').click(function(){
+        $('#hamburger').toggleClass('open');
+        $('.responsive-navigation').toggleClass('visible');
+    });
+});
