c4279384f8955d3ecc9cf9c7ce10130da4f6f482
fervandisseldorp <fervandisseldorp@gmail.com>
2017-04-02 16:36:32 +0200

    attractionStyling

--- src/main/java/B2a/controller/AttractionController.java
+++ src/main/java/B2a/controller/AttractionController.java
@@ -44,14 +44,6 @@ public class AttractionController {
     }
 
 
-//    @RequestMapping(value = "attraction/adminAttractionsList", method = RequestMethod.POST)
-//    public ModelAndView adminAttractionList(Model model, @RequestParam(value="action", required = true) String action){
-//        Iterable<Attraction> attractions = attractionManagerIF.findAllAttractions();
-//        System.out.println("test");
-//
-//        return new ModelAndView("/attraction/adminAttractionsList", "attractions", attractions);
-//    }
-
     @RequestMapping(value = "attraction/adminAttractionsList", method = RequestMethod.POST)
     public String adminAttractionsList(Rollercoaster attraction, @RequestParam(value="action", required = true) String action){
         //Parameter should be Attraction, but can't instantiate abstract class..
@@ -59,13 +51,6 @@ public class AttractionController {
         attractionManagerIF.changeState(attraction, action);
         return "redirect:/attraction/adminAttractionsList";
     }
-
-//
-//    @RequestMapping(value = "/attraction/attractionAdmin", method = RequestMethod.GET)
-//    public ModelAndView attractionAdmin(Attraction attraction) {
-//
-//        return new ModelAndView("attractionAdmin", "Attraction", null);
-//    }
 
 
 //    @RequestMapping(value = "attraction/attractionForm", method = RequestMethod.GET)
@@ -100,7 +85,7 @@ public class AttractionController {
 
 
     @RequestMapping(value = "/attraction/attractionChooser", method = RequestMethod.GET)
-    public String attractionChooser(){
+    public String attractionChooser(Model model){
         return "attraction/attractionChooser";
     }
 
@@ -108,7 +93,7 @@ public class AttractionController {
     public String attractionChooser(Model model, @RequestParam(value="action", required = true) String action) {
         attractionManagerIF.createNewAttraction(action);
 
-        return "redirect:/attraction/attractionsList";
+        return "redirect:/attraction/adminAttractionsList";
     }
 
 //
--- src/main/java/B2a/controller/TicketController.java
+++ src/main/java/B2a/controller/TicketController.java
@@ -1,7 +1,7 @@
 package B2a.controller;
 
 import B2a.model.TicketModel;
-import B2a.domain.Ticket.*;
+import B2a.domain.ticket.*;
 import B2a.repository.BaseTicketRepository;
 import B2a.service.abstractService.TicketManagerIF;
 import B2a.service.concreteService.TicketManager;
@@ -20,62 +20,62 @@ import java.util.List;
 @Controller
 public class TicketController {
 
-    @Autowired
-    private final BaseTicketRepository baseTicketRepository;
-    private TicketManagerIF manager;
-    private TicketModel ticketModel;
-
-    public TicketController(BaseTicketRepository baseTicketRepository) {
-        this.ticketModel = new TicketModel();
-        this.baseTicketRepository = baseTicketRepository;
-        this.manager = new TicketManager(baseTicketRepository);
-    }
-
-    private void createTicket(TicketModel model) {
-        manager.createTicket(model);
-    }
-    private void decorateTicket(TicketModel model) {
-        manager.decorateTicket(model);
-    }
-
-
-    /////////////////PAS AAN DEZE PAGINA///////////////////////////////
-    @RequestMapping(value = "/ticketOrder", method = RequestMethod.GET)
-    public ModelAndView ticketOrder() {
-        return new ModelAndView("ticketOrder", "Ticket", null);
-    }
-
-
-    /////////////////PAS AAN DEZE PAGINA///////////////////////////////
-    @RequestMapping(value = "/ticketOrder", method = RequestMethod.POST)
-    public ModelAndView ticketOrder(Ticket ticket) {
-
-        return new ModelAndView("ticketOrderForm", "Ticket", ticket);
-    }
-
-    @RequestMapping(value = "/ticketOrderForm", method = RequestMethod.GET)
-    public ModelAndView ticketOrderForm(Ticket ticket) {
-
-        List<TicketOption> option = new ArrayList<TicketOption>();
-        option.add(new TicketOption("Lunch", 5, "description", "22-58-86", ticket));
-        option.add(new TicketOption("Cadeau", 75, "description", "22-58-86", ticket));
-
-        ticketModel.setOption(option);
-        ticketModel.setTicket(ticket);
-        return new ModelAndView("ticketOrderForm", "ticketModel", ticketModel);
-    }
-
-    @RequestMapping(value = "/ticketOrderForm", method = RequestMethod.POST)
-    public ModelAndView ticketOrderForm(@ModelAttribute("ticketModel") TicketModel ticketModel, BindingResult bindingResult, Model model) {
-
-        if (bindingResult.hasErrors()) {
-            return new ModelAndView("ticketOrderForm", "ticketModel", ticketModel);
-       }
-
-        System.out.println(ticketModel.toString());
-
-        //createTicket(ticketModel);
-        //decorateTicket(ticketModel);
-        return new ModelAndView("ticketOrder", "ticketModel", ticketModel);
-    }
+//    @Autowired
+//    private final BaseTicketRepository baseTicketRepository;
+//    private TicketManagerIF manager;
+//    private TicketModel ticketModel;
+//
+//    public TicketController(BaseTicketRepository baseTicketRepository) {
+//        this.ticketModel = new TicketModel();
+//        this.baseTicketRepository = baseTicketRepository;
+//        this.manager = new TicketManager(baseTicketRepository);
+//    }
+//
+//    private void createTicket(TicketModel model) {
+//        manager.createTicket(model);
+//    }
+//    private void decorateTicket(TicketModel model) {
+//        manager.decorateTicket(model);
+//    }
+//
+//
+//    /////////////////PAS AAN DEZE PAGINA///////////////////////////////
+//    @RequestMapping(value = "/ticketOrder", method = RequestMethod.GET)
+//    public ModelAndView ticketOrder() {
+//        return new ModelAndView("ticketOrder", "Ticket", null);
+//    }
+//
+//
+//    /////////////////PAS AAN DEZE PAGINA///////////////////////////////
+//    @RequestMapping(value = "/ticketOrder", method = RequestMethod.POST)
+//    public ModelAndView ticketOrder(Ticket ticket) {
+//
+//        return new ModelAndView("ticketOrderForm", "Ticket", ticket);
+//    }
+//
+//    @RequestMapping(value = "/ticketOrderForm", method = RequestMethod.GET)
+//    public ModelAndView ticketOrderForm(Ticket ticket) {
+//
+//        List<TicketOption> option = new ArrayList<TicketOption>();
+//        option.add(new TicketOption("Lunch", 5, "description", "22-58-86", ticket));
+//        option.add(new TicketOption("Cadeau", 75, "description", "22-58-86", ticket));
+//
+//        ticketModel.setOption(option);
+//        ticketModel.setTicket(ticket);
+//        return new ModelAndView("ticketOrderForm", "ticketModel", ticketModel);
+//    }
+//
+//    @RequestMapping(value = "/ticketOrderForm", method = RequestMethod.POST)
+//    public ModelAndView ticketOrderForm(@ModelAttribute("ticketModel") TicketModel ticketModel, BindingResult bindingResult, Model model) {
+//
+//        if (bindingResult.hasErrors()) {
+//            return new ModelAndView("ticketOrderForm", "ticketModel", ticketModel);
+//       }
+//
+//        System.out.println(ticketModel.toString());
+//
+//        //createTicket(ticketModel);
+//        //decorateTicket(ticketModel);
+//        return new ModelAndView("ticketOrder", "ticketModel", ticketModel);
+//    }
 }
